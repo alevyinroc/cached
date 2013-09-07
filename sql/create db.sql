fb5853999fd589462582e502ef7cb10f01cdbd38
Andy Levy <alevyinroc@users.noreply.github.com>
2013-09-07 16:01:26 -0400

    Skeleton

---
+++ sql/create db.sql
@@ -0,0 +1,20 @@
+create table caches as (
+)
+create table attributes as (
+)
+create table cache_attributes as (
+)
+create table owners as (
+)
+create table cache_owners as (
+)
+create table waypoints as (
+)
+create table waypoint_types as (
+)
+create table cache_sizes as (
+)
+create table logs as (
+)
+create table cache_logs as (
+)
