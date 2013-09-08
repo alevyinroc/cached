2fe5c47c2c1ee6b92f3ea926127c19795051a13f
Andy Levy <andy@levyclan.us>
2013-09-08 17:14:46 -0400

    Adding FK Constraints

--- sql/create db.sql
+++ sql/create db.sql
@@ -19,6 +19,8 @@ create table caches as (
 	hint UNKNOWN NULL,
 	available bit not null default 1,
 	archived bit not null default 0
+	constraint fk_cache_type foreign key (typeid) references point_types (typeid),
+	constraing fk_container_type foreign key (containerid) references cache_containers (containerid)
 )
 create table attributes as (
 	attributeid int not null unique,
@@ -28,6 +30,8 @@ create table cache_attributes as (
 	cacheid varchar(8) not null,
 	attributeid int not null,
 	attribute_applies bit default 1 /* Attributes can be tagged to a cache as "yes" or "no". For example a cache may have the "poisonous plants" attribute set, but flagged as "no poisonous plants" indicating that there isn't poison ivy at a cache site. If the inc attribute of the <groundspeak:attribute> element is 1, this indicates "yes".*/
+	constraint fk_attr_cacheid foreign key (cacheid) references caches (cacheid),
+	constraint fk_attr_id foreign key (attributeid) references attributes (attributeid)
 )
 create table cachers as (
 	cacherid int not null unique,
@@ -36,6 +40,8 @@ create table cachers as (
 create table cache_owners as (
 	cacheid varchar(8),
 	cacherid int
+	constraint fk_owner_cacheid foreign key (cacheid) references caches (cacheid),
+	constraint fk_owner_cacher foreign key (cacheid) references caches (cacheid)
 )
 create table waypoints as (
 	waypointid varchar(8) not null unique,
@@ -48,7 +54,8 @@ create table waypoints as (
 	description varchar() not null,
 	comment varchar(),
 	url varchar(200) not null
-	
+	constraint fk_waypoint_cacheid foreign key (parentcache) references caches (cacheid),
+	constraint fk_waypoint_type foreign key (typeid) references point_types (typeid)
 )
 create table point_types as (
 	typeid int not null unique,
@@ -71,21 +78,26 @@ create table logs as (
 	logtext UNKNOWN,
 	latitude float null,
 	longitude float null,
-	latlong geography null
+	latlong geography null,
+	foreign key (fk_logtype) references log_types (logtypeid),
+	foreign key (fk_cacher) references cachers (cacherid)
 )
 create table cache_logs as (
 	cacheid varchar(8) not null,
-	logid bigint not null
+	logid bigint not null,
+	foreign key (fk_log_cacheid) references caches (cacheid),
+	foreign key (fk_log_logid) references logs (logid)
 )
 create table log_types as (
 	logtypeid int not null unique,
 	logtypedesc varchar(20) not null
 )
 create table tbinventory as (
-	cacheid varchar(8) not null
+	cacheid varchar(8) not null,
 	tbid UNKNOWN not null
+	constraint fk_tb_id foreign key (tbid) references travelbugs (tbid)
 )
-create table travelbungs as (
+create table travelbugs as (
 	tbid UNKNOWN not null unique
 	tbname varchar() not null
 )
