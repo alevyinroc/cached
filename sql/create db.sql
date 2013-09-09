63ac085c4c6d077b76e6a1ec64ef6c304cf90b96
Andy Levy <andy@levyclan.us>
2013-09-08 23:44:00 -0400

    Initial database creation

--- sql/create db.sql
+++ sql/create db.sql
@@ -1,3 +1,14 @@
+create database Geocaches;
+alter database Geocaches modify file (
+	(NAME = N'Geocaches', SIZE = 100MB , FILEGROWTH = 2MB);
+);
+alter database Geocaches modify file (
+	(NAME = N'Geocaches_log', SIZE = 100MB , FILEGROWTH = 2MB);
+);
+ALTER DATABASE Geocaches SET RECOVERY FULL;
+ALTER DATABASE Geocaches SET AUTO_UPDATE_STATISTICS ON;
+ALTER DATABASE Geocaches SET AUTO_UPDATE_STATISTICS_ASYNC ON;
+
 create table caches as (
 	cacheid varchar(8) not null constraint pk_caches primary key,
 	gsid int not null
