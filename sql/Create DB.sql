995411014cdcff92eae5859b2ce6eedebec56a39
Andy Levy <alevyinroc@users.noreply.github.com>
2013-09-09 21:37:46 -0400

    Schema setup

---
+++ sql/Create DB.sql
@@ -0,0 +1,15 @@
+ALTER DATABASE Geocaches
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+drop database geocaches;
+
+create database Geocaches;
+alter database Geocaches modify file 
+	(NAME = N'Geocaches', SIZE = 100MB , FILEGROWTH = 2MB);
+
+alter database Geocaches modify file
+	(NAME = N'Geocaches_log', SIZE = 100MB , FILEGROWTH = 2MB);
+
+ALTER DATABASE Geocaches SET RECOVERY FULL;
+ALTER DATABASE Geocaches SET AUTO_UPDATE_STATISTICS ON;
+ALTER DATABASE Geocaches SET AUTO_UPDATE_STATISTICS_ASYNC ON;
