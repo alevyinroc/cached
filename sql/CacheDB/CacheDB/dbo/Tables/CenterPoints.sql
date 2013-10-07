a19f38cd8ba1c6e2e256115b97db6c0b18f4470d
Andy Levy <alevyinroc@users.noreply.github.com>
2013-10-06 21:38:55 -0400

    fixes #25

--- sql/CacheDB/CacheDB/dbo/Tables/CenterPoints.sql
+++ sql/CacheDB/CacheDB/dbo/Tables/CenterPoints.sql
@@ -1,6 +1,12 @@
 ﻿CREATE TABLE [dbo].[CenterPoints] (
-    [Locationname] VARCHAR (20)      NOT NULL,
-    [LocationGeo]  [sys].[geography] NOT NULL,
+    [Locationname] VARCHAR (20) NOT NULL,
+    [Latitude]     FLOAT (53)   NULL,
+    [Longitude]    FLOAT (53)   NULL,
+    [latlong]      AS           ([geography]::Point([latitude],[longitude],(4326))) PERSISTED,
     CONSTRAINT [pk_centerpoints] PRIMARY KEY CLUSTERED ([Locationname] ASC)
 );
+
+
+
+
 
