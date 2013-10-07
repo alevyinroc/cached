3a43cad20093b2d4c488478d35404476d04837c4
dakboy <dakboy@localhost>
2013-10-06 20:59:27 -0400

    fixes #17

--- sql/CacheDB/CacheDB/dbo/Tables/statuses.sql
+++ sql/CacheDB/CacheDB/dbo/Tables/statuses.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[statuses] (
     [statusid]   INT          DEFAULT (NEXT VALUE FOR [statusid]) NOT NULL,
-    [statusname] VARCHAR (12) NOT NULL,
+    [statusname] NVARCHAR(12) NOT NULL,
     CONSTRAINT [pk_statuses] PRIMARY KEY CLUSTERED ([statusid] ASC)
 );
 
