7ad0bbc0de329e86da44bc86d75b8fb3fcaec344
dakboy <dakboy@localhost>
2013-10-06 21:22:54 -0400

    Clean up database loose ends

--- sql/CacheDB/CacheDB/dbo/Tables/cache_attributes.sql
+++ sql/CacheDB/CacheDB/dbo/Tables/cache_attributes.sql
@@ -3,6 +3,7 @@
     [attributeid]       INT         NOT NULL,
     [attribute_applies] BIT         DEFAULT ((1)) NULL,
     CONSTRAINT [fk_attr_cacheid] FOREIGN KEY ([cacheid]) REFERENCES [dbo].[caches] ([cacheid]),
-    CONSTRAINT [fk_attr_id] FOREIGN KEY ([attributeid]) REFERENCES [dbo].[attributes] ([attributeid])
+    CONSTRAINT [fk_attr_id] FOREIGN KEY ([attributeid]) REFERENCES [dbo].[attributes] ([attributeid]), 
+    CONSTRAINT [PK_cache_attributes] PRIMARY KEY ([cacheid], [attributeid])
 );
 
