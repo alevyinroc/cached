b0108b0efe8b0f3a2c7e2177f8aa8e29b0c01a65
dakboy <dakboy@localhost>
2013-10-13 21:54:05 -0400

    Fix SSDT project

--- sql/CacheDB/CacheDB/dbo/Tables/CenterPoints.sql
+++ sql/CacheDB/CacheDB/dbo/Tables/CenterPoints.sql
@@ -2,7 +2,7 @@
     [Locationname] VARCHAR (20) NOT NULL,
     [Latitude]     FLOAT (53)   NULL,
     [Longitude]    FLOAT (53)   NULL,
-    [latlong]      AS           ([geography]::Point([latitude],[longitude],(4326))) PERSISTED,
+    [latlong]      AS           ([geography]::Point([Latitude],[Longitude],(4326))) PERSISTED,
     CONSTRAINT [pk_centerpoints] PRIMARY KEY CLUSTERED ([Locationname] ASC)
 );
 
