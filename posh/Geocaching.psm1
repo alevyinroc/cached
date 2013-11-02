bdd42f3bc95436e005dd8098b8cd87f2071a3654
Andy Levy <alevyinroc@users.noreply.github.com>
2013-11-02 14:30:36 -0400

    Work in progress for corrected coords re #30, new module re #35

---
+++ posh/Geocaching.psm1
@@ -0,0 +1,79 @@
+﻿function Set-CorrectedCoordinates {
+<#
+.SYNOPSIS
+	Adds/updates corrected coordinates for a cache
+.DESCRIPTION
+	Adds/updates corrected coordinates for a cache. Confirmation is required to overwrite pre-existing corrected coordinates for the cache.
+.PARAMETER CacheId
+	Public ID (GC code) of the cache to be updated
+.PARAMETER Latitude
+	Corrected latitude
+.PARAMETER Longitude
+	Corrected longitude
+.PARAMETER CorrectedDetails
+	A PSObject containing the following properties:
+		CacheId (string)
+		Latitude (float)
+		Longitude (float)
+.PARAMETER SQLInstance
+	SQL Server instance to hosting the database
+.PARAMETER Database
+	Database on the SQLInstance hosting the geocache database
+.EXAMPLE
+#>
+[cmdletbinding(SupportsShouldProcess=$True,ConfirmImpact='High')]
+param(
+	[Parameter(Mandatory=$true,ParameterSetName="ExplicitCacheDetails")]
+	[string]$CacheId,
+	[Parameter(Mandatory=$true,ParameterSetName="ExplicitCacheDetails")]
+	[alias("Lat")]
+	[float]$Latitude,
+	[Parameter(Mandatory=$true,ParameterSetName="ExplicitCacheDetails")]
+	[alias("Long")]
+	[float]$Longitude,
+	[Parameter(Mandatory=$true,ParameterSetName="UpdateObject",ValueFromPipeline=$true)]
+	[PSObject[]]$CorrectedDetails,
+	[Parameter(Mandatory=$true)]
+	[ValidateScript({Test-Connection -computername $_.Split('\')[0] -quiet})]
+	[string]$SQLInstance = 'Hobbes\sqlexpress',
+	[Parameter(Mandatory=$true)]
+	[string]$Database = 'Geocaches'
+)
+	begin {
+		$SQLConnectionString = "Server=$SQLInstance;Database=$Database;Trusted_Connection=True;Application Name=Geocache Loader;";
+		$SQLConnection = new-object System.Data.SqlClient.SqlConnection;
+		$SQLConnection.ConnectionString = $SQLConnectionString;
+		$SQLConnection.Open();
+		$UpdateCoordsCmd = $SQLConnection.CreateCommand();
+		$UpdateCoordsCmd.CommandText = "update caches set CorrectedLatitude = @lat, CorrectedLongitude = @long where CacheId = @cacheid;";
+		$UpdateCoordsCmd.Parameters.Add("@cacheid", [System.Data.SqlDbType]::Char, 8) | Out-Null;
+		$UpdateCoordsCmd.Parameters.Add("@lat", [System.Data.SqlDbType]::Float) | Out-Null;
+		$UpdateCoordsCmd.Parameters.Add("@long", [System.Data.SqlDbType]::Float) | Out-Null;
+		$UpdateCoordsCmd.Prepare();
+		$CheckForCorrectedCmd = $SQLConnection.CreateCommand();
+		$CheckForCorrectedCmd.CommandText = "select 1 from caches where cacheid = @cacheid and CorrectedLatitude is not null and CorrectedLongitude is not null;";
+		$CheckForCorrectedCmd.Parameters.Add("@cacheid",[System.Data.SqlDbType]::Char, 8) | Out-Null;
+		$CheckForCorrectedCmd.Prepare();
+	}
+	process {
+		switch ($PsCmdlet.ParameterSetName) {
+			"UpdateObject" {$CacheId = $CorrectedDetails.CacheId;$Latitude= $CorrectedDetails.Latitude;$Longitude= $CorrectedDetails.Longitude;}
+		}
+		$CheckForCorrectedCmd.Parameters["@cacheid"].Value = $CacheId;
+		$HasCorrected = $CheckForCorrectedCmd.ExecuteScalar();
+		if ($HasCorrected -and $pscmdlet.ShouldProcess()) {
+			$UpdateCoordsCmd.Parameters["@cacheid"].Value = $CacheId;
+			$UpdateCoordsCmd.Parameters["@lat"].Value = $Latitude;
+			$UpdateCoordsCmd.Parameters["@long"].Value = $Longitude;
+			$UpdateCoordsCmd.ExecuteNonQuery();
+		}
+	}		
+	end {
+		$CheckForCorrectedCmd.Dispose();
+		$UpdateCoordsCmd.Dispose();
+		$SQLConnection.Close();
+		$SQLConnection.Dispose();
+	}
+}
+
+Export-ModuleMember Set-CorrectedCoordinates
