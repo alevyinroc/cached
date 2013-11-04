5a7a068f9adfe15a2408e4ed7f1af7f9dc3630d0
Andy Levy <alevyinroc@users.noreply.github.com>
2013-11-03 20:45:10 -0500

    State & country stuff works. Fixes #33

---
+++ posh/Modules/Geocaching/Geocaching.psm1
@@ -0,0 +1,162 @@
+﻿function Set-CorrectedCoordinates {
+<#
+.SYNOPSIS
+	Adds/updates corrected coordinates for a cache
+.DESCRIPTION
+	Adds/updates corrected coordinates for a cache. Confirmation is required to overwrite pre-existing corrected coordinates for the cache.
+.PARAMETER CacheId
+	Public ID (GC code) of the cache to be updated
+.PARAMETER Latitude
+	Corrected latitude
+.PARAMETER Longitude
+	Corrected longitude
+.PARAMETER CorrectedDetails
+	A PSObject containing the following properties:
+		CacheId (string)
+		Latitude (float)
+		Longitude (float)
+.PARAMETER SQLInstance
+	SQL Server instance to hosting the database
+.PARAMETER Database
+	Database on the SQLInstance hosting the geocache database
+.EXAMPLE
+#>
+[cmdletbinding(SupportsShouldProcess=$True,ConfirmImpact='High')]
+param(
+	[Parameter(Mandatory=$true,ParameterSetName="ExplicitCacheDetails")]
+	[string]$CacheId,
+	[Parameter(Mandatory=$true,ParameterSetName="ExplicitCacheDetails")]
+	[alias("Lat")]
+	[float]$Latitude,
+	[Parameter(Mandatory=$true,ParameterSetName="ExplicitCacheDetails")]
+	[alias("Long")]
+	[float]$Longitude,
+	[Parameter(Mandatory=$true,ParameterSetName="UpdateObject",ValueFromPipeline=$true)]
+	[PSObject[]]$CorrectedDetails,
+	[Parameter(Mandatory=$true)]
+	[ValidateScript({Test-Connection -computername $_.Split('\')[0] -quiet})]
+	[string]$SQLInstance = 'Hobbes\sqlexpress',
+	[Parameter(Mandatory=$true)]
+	[string]$Database = 'Geocaches'
+)
+	begin {
+		$SQLConnectionString = "Server=$SQLInstance;Database=$Database;Trusted_Connection=True;Application Name=Geocache Loader;";
+		$SQLConnection = new-object System.Data.SqlClient.SqlConnection;
+		$SQLConnection.ConnectionString = $SQLConnectionString;
+		$SQLConnection.Open();
+		$UpdateCoordsCmd = $SQLConnection.CreateCommand();
+		$UpdateCoordsCmd.CommandText = "update caches set CorrectedLatitude = @lat, CorrectedLongitude = @long where CacheId = @cacheid;";
+		$UpdateCoordsCmd.Parameters.Add("@cacheid", [System.Data.SqlDbType]::Char, 8) | Out-Null;
+		$UpdateCoordsCmd.Parameters.Add("@lat", [System.Data.SqlDbType]::Float) | Out-Null;
+		$UpdateCoordsCmd.Parameters.Add("@long", [System.Data.SqlDbType]::Float) | Out-Null;
+		$UpdateCoordsCmd.Prepare();
+		$CheckForCorrectedCmd = $SQLConnection.CreateCommand();
+		$CheckForCorrectedCmd.CommandText = "select 1 from caches where cacheid = @cacheid and CorrectedLatitude is not null and CorrectedLongitude is not null;";
+		$CheckForCorrectedCmd.Parameters.Add("@cacheid",[System.Data.SqlDbType]::Char, 8) | Out-Null;
+		$CheckForCorrectedCmd.Prepare();
+	}
+	process {
+		switch ($PsCmdlet.ParameterSetName) {
+			"UpdateObject" {$CacheId = $CorrectedDetails.CacheId;$Latitude= $CorrectedDetails.Latitude;$Longitude= $CorrectedDetails.Longitude;}
+		}
+		$CheckForCorrectedCmd.Parameters["@cacheid"].Value = $CacheId;
+		$HasCorrected = $CheckForCorrectedCmd.ExecuteScalar();
+		if ($HasCorrected -and $pscmdlet.ShouldProcess()) {
+			$UpdateCoordsCmd.Parameters["@cacheid"].Value = $CacheId;
+			$UpdateCoordsCmd.Parameters["@lat"].Value = $Latitude;
+			$UpdateCoordsCmd.Parameters["@long"].Value = $Longitude;
+			$UpdateCoordsCmd.ExecuteNonQuery();
+		}
+	}		
+	end {
+		$CheckForCorrectedCmd.Dispose();
+		$UpdateCoordsCmd.Dispose();
+		$SQLConnection.Close();
+		$SQLConnection.Dispose();
+	}
+}
+
+function New-StateCountryId {
+<#
+.SYNOPSIS
+	Adds a new state or country to the database
+.DESCRIPTION
+	Adds a new state or country to the database and returns its ID.
+.PARAMETER Name
+	Name of the new state or country to add
+.PARAMETER Type
+	Type of location to add
+.PARAMETER SQLInstance
+	SQL Server instance to hosting the database
+.PARAMETER Database
+	Database on the SQLInstance hosting the geocache database
+.EXAMPLE
+#>
+[cmdletbinding(SupportsShouldProcess=$False)]
+param(
+	[Parameter(Mandatory=$true)]
+	[string]$Name,
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("State","Country")]
+	[string]$Type,
+	[Parameter(Mandatory=$true)]
+	[ValidateScript({Test-Connection -computername $_.Split('\')[0] -quiet})]
+	[string]$SQLInstance = 'Hobbes\sqlexpress',
+	[Parameter(Mandatory=$true)]
+	[string]$Database = 'Geocaches'
+)
+
+	$SQLConnectionString = "Server=$SQLInstance;Database=$Database;Trusted_Connection=True;Application Name=Geocache Loader;";
+	$SQLConnection = new-object System.Data.SqlClient.SqlConnection;
+	$SQLConnection.ConnectionString = $SQLConnectionString;
+	$SQLConnection.Open();
+	$NewStateCountryCmd = $SQLConnection.CreateCommand();
+	$NewStateCountryCmd.Parameters.Add("@Name", [System.Data.SqlDbType]::NChar, 50) | Out-Null;
+	$GetIdCmd = $SQLConnection.CreateCommand();
+	$GetIdCmd.Parameters.Add("@Name", [System.Data.SqlDbType]::NChar, 50) | Out-Null;
+	switch ($Type) {
+		"State" {$NewStateCountryCmd.CommandText = "insert into States (Name) values (@Name);";$GetIdCmd.CommandText = "select StateId from States where Name = @Name;"}
+		"Country"{$NewStateCountryCmd.CommandText = "insert into Countries (Name) values (@Name);";$GetIdCmd.CommandText = "select CountryId from Countries where Name = @Name;"}
+	}
+	$NewStateCountryCmd.Prepare();
+	$GetIdCmd.Prepare();
+	$NewStateCountryCmd.Parameters["@Name"].Value = $Name;
+	$GetIdCmd.Parameters["@Name"].Value = $Name;
+	$NewStateCountryCmd.ExecuteNonQuery() | Out-Null;
+	$NewId = $GetIdCmd.ExecuteScalar();
+	$NewStateCountryCmd.Dispose();
+	$GetIdCmd.Dispose();
+	$SQLConnection.Close();
+	$SQLConnection.Dispose();
+	$NewId;
+}
+
+function Get-StateLookups {
+[cmdletbinding(SupportsShouldProcess=$False)]
+param (
+	[Parameter(Mandatory=$true)]
+	[ValidateScript({Test-Connection -computername $_.Split('\')[0] -quiet})]
+	[string]$SQLInstance = 'Hobbes\sqlexpress',
+	[Parameter(Mandatory=$true)]
+	[string]$Database = 'Geocaches'
+)
+	$StateLookup = Invoke-SQLCmd -server $SQLInstance -database $Database -query "select StateId, rtrim(ltrim(Name)) as Name from states order by StateId Desc;";
+	$StateLookup;
+}
+function Get-CountryLookups {
+[cmdletbinding(SupportsShouldProcess=$False)]
+param (
+	[Parameter(Mandatory=$true)]
+	[ValidateScript({Test-Connection -computername $_.Split('\')[0] -quiet})]
+	[string]$SQLInstance = 'Hobbes\sqlexpress',
+	[Parameter(Mandatory=$true)]
+	[string]$Database = 'Geocaches'
+)
+	$CountryLookup = Invoke-SQLCmd -server $SQLInstance -database $Database -query "select CountryId, rtrim(ltrim(Name)) as Name from Countries order by CountryId Desc;";
+	$CountryLookup;
+}
+
+Export-ModuleMember Set-CorrectedCoordinates
+Export-ModuleMember New-StateCountryId
+Export-ModuleMember Get-StateLookups
+Export-ModuleMember Get-CountryLookups
---
+++ sql/CacheDB/CacheDB/dbo/Tables/logs.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[logs] (
+    [logid]     BIGINT          NOT NULL,
+    [logdate]   DATETIME        NOT NULL,
+    [logtypeid] INT             NOT NULL,
+    [cacherid]  INT             NOT NULL,
+    [logtext]   NVARCHAR (4000) NULL,
+    [latitude]  FLOAT (53)      NULL,
+    [longitude] FLOAT (53)      NULL,
+    [latlong]   AS              ([geography]::Point([latitude],[longitude],(4326))) PERSISTED,
+    CONSTRAINT [pk_logs] PRIMARY KEY CLUSTERED ([logid] ASC),
+    CONSTRAINT [fk_cacher] FOREIGN KEY ([cacherid]) REFERENCES [dbo].[cachers] ([cacherid]),
+    CONSTRAINT [fk_logtype] FOREIGN KEY ([logtypeid]) REFERENCES [dbo].[log_types] ([logtypeid])
+);
+
