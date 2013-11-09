b20334de5dc8ea5cb6d794323e28bbfce04648f9
Andy Levy <alevyinroc@users.noreply.github.com>
2013-11-09 07:58:57 -0500

    Fix up GPX extraction

--- posh/Get-GPXFromPQs.ps1
+++ posh/Get-GPXFromPQs.ps1
@@ -10,7 +10,7 @@ param (
 
 function Expand-ZIPFile {
 param (
-	$file
+	$file,
 	$destination
 )
 <#
@@ -31,23 +31,27 @@ param (
 	[string]$DirWithPQs = 'C:\Users\andy\Documents\Code\cachedb\scratchpad\'
 )
 	$GPXPath = Join-Path -Path $Env:TEMP -ChildPath "PQs";
-	New-Item -ItemType Directory -Path $GPXPath | Out-Null;
+	if (-not (Test-Path -Path $GPXPath -PathType Container)) {
+		New-Item -ItemType Directory -Path $GPXPath | Out-Null;
+	}
 	while (-not (Test-Path -Path $GPXPath -PathType Container)){
 		Start-Sleep -Seconds 1;
 	}
 	Get-ChildItem -Path $DirWithPQs -Filter *.gpx | Copy-Item -Destination $GPXPath;
-	$Zipfiles = Get-ChildItem -Path $DirWithPQs -Filter *.zip;
-	foreach ($zip in $Zipfiles) {
-		Expand-Zipfile -file $zip.FullName -destination $GPXPath;
-	}
+	Get-ChildItem -Path $DirWithPQs -Filter *.zip | foreach {Expand-Zipfile -file $_.FullName -destination $GPXPath;};
 	$AllGPXFiles = Get-ChildItem -Path $GPXPath -Filter *.gpx;
 	$GPXWithDate = @();
 	foreach ($gpx in $AllGPXFiles) {
+	# Not using DateTimeOffset here because we're not concerned about the exact time, just the relative times.
+	# Todo: Find more efficient way to search large files
 		$FileTime = (select-string -Path $gpx.FullName -pattern "(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))" -list).Matches[0].value;
-		$FileData = New-Object -TypeName PSObject;
-		$FileData|Add-Member -Name "FilePath" -Value $gpx.FullName -MemberType NoteProperty;
-		$FileData|Add-Member -Name "Timestamp" -Value $FileTime -MemberType NoteProperty;
+		$FileData = New-Object -TypeName PSObject -Property @{
+			FilePath = $gpx.FullName
+			Timestamp =  $FileTime	
+		};
 		$GPXWithDate+=$FileData;
 	}
 	$GPXWithDate|Sort-Object -Property Timestamp;
 }
+
+Get-GPXFiles -DirWithPQs $DirWithPQs;
