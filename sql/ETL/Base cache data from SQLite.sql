c960149c9618bd2124e6f22763383641d0522d18
Andy Levy <alevyinroc@users.noreply.github.com>
2013-11-23 12:03:52 -0500

    Starting SQL ETL process re #42

---
+++ sql/ETL/Base cache data from SQLite.sql
@@ -0,0 +1,30 @@
+SELECT C.code as cacheid,
+       cacheid AS gsid,
+       C.name AS cachename,
+       C.latoriginal AS latitude,
+       C.lonoriginal AS longitude,
+       C.lastgpxdate AS lastupdated,
+       C.PlacedDate AS placed,
+       C.PlacedBy,
+       C.CacheType AS TypeId,
+       C.Container AS sizeid,
+       C.difficulty,
+       C.terrain,
+       cm.shortdescription AS shortdesc,
+       cm.longdescription AS longdesc,
+       cm.hints AS hint,
+       'available' AS available,
+       'archived' AS archived,
+       C.ispremium AS premiumonly,
+       'cachestatus' AS cachestatus,
+       C.created,
+       cm.url AS url,
+       'urldesc' AS urldesc,
+       C.latitude AS correctedlatitude,
+       C.longitude AS correctedlongitude,
+       C.country AS countryid,
+       C.state AS stateid
+  FROM Caches C
+       INNER JOIN cachememo CM
+               ON C.code = cm.code;
+
