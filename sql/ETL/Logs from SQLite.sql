93efb8c93ddba33e0dacc173d4c5024d7991801d
Andy Levy <andy@levyclan.us>
2013-11-23 12:03:52 -0500

    Starting SQL ETL process re #42

---
+++ sql/ETL/Logs from SQLite.sql
@@ -0,0 +1,8 @@
+﻿select L.llogid as logid,
+L.ldate + L.ltime as logdate,
+L.ltype as logtypeid,
+L.lownerid as cacherid,
+LM.ltext as logtext,
+llat as latitude,
+llong as longitude
+from logs L inner join logmemo LM on l.llogid = lm.llogid
