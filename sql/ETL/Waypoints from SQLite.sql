4a0c84cb5a23973e7c026e2045c207f45b692ae9
Andy Levy <andy@levyclan.us>
2013-11-24 08:29:18 -0500

    Starting SQL ETL process re #42

---
+++ sql/ETL/Waypoints from SQLite.sql
@@ -0,0 +1,15 @@
+SELECT w.ccode AS waypointid,
+       w.cparent AS parentcache,
+       w.clat AS latitude,
+       w.clon AS longitude,
+       w.ctype AS typeid,
+       w.cname AS name,
+       wm.ccomment AS description,
+       wm.curl AS url,
+       w.cdate AS lastupdated
+  FROM waypoints W
+       INNER JOIN waymemo WM
+               ON W.ccode = wm.ccode 
+AND
+w.cparent = wm.cparent;
+
