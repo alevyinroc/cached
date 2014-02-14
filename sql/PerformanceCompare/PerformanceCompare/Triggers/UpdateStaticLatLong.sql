ee4bc65b64208f93625c7cec1aa0af08b581215e
alroc <andy@levyclan.us>
2014-02-13 23:10:22 -0500

    Fixing

--- sql/PerformanceCompare/PerformanceCompare/Triggers/UpdateStaticLatLong.sql
+++ sql/PerformanceCompare/PerformanceCompare/Triggers/UpdateStaticLatLong.sql
@@ -3,6 +3,9 @@ ON TriggerGeoColumn
 after insert, update
 AS
 BEGIN
-	SET NOCOUNT ON
-	
+	SET NOCOUNT ON   
+
+	declare @newlat float(53),@newlong float(53)
+	select @newlat = latitude, @newlong = longitude	 from INSERTED;
+	update T set latlong = GetGeoPoint(@newlat,@newlong) from TriggerGeoColumn as T WHERE EXISTS (SELECT 1 FROM inserted WHERE ID = t.ID);
 END
