4974d8a14002bb2349c9a93b02ca7562ce14b6a6
Andy Levy <alevyinroc@users.noreply.github.com>
2014-02-13 23:10:22 -0500

    Fixing

---
+++ sql/PerformanceCompare/PerformanceCompare/Functions/GeoPoint.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[GeoPoint]
+(
+	@lat float(53),
+	@long float(53)
+)
+RETURNS geography
+AS
+BEGIN
+	RETURN geography::STGeomFromText('Point(' + cast(@long as varchar(20)) + ' ' + cast(@lat as varchar(20)) + ')',4326);
+END
