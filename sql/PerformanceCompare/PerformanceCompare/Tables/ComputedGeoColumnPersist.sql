1bb0a602d16f95117422101587b8bfb623bf215f
alroc <andy@levyclan.us>
2014-02-13 22:50:48 -0500

    Setting up for performance comparisons

---
+++ sql/PerformanceCompare/PerformanceCompare/Tables/ComputedGeoColumnPersist.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ComputedColumnPersist]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	    [latitude]    FLOAT (53)      NOT NULL,
+    [longitude]   FLOAT (53)      NOT NULL,
+	[latlong]     AS              GeoPoint(longitude,latitude) PERSISTED
+		check (latitude >= -90 and latitude <= 90)
+	check (longitude >= -180 and longitude <= 180)
+)
---
+++ sql/PerformanceCompare/PerformanceCompare/Tables/NoGeoColumn.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[NoGeoColumn]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[latitude]    FLOAT (53)      NOT NULL,
+    [longitude]   FLOAT (53)      NOT NULL
+	check (latitude >= -90 and latitude <= 90)
+	check (longitude >= -180 and longitude <= 180)
+)
