1e661f7a21f1ee65a54010ee30270690bcbc48e0
Andy Levy <alevyinroc@users.noreply.github.com>
2014-03-01 08:44:58 -0500

    Still working on database connection consistency

---
+++ posh/Bulkload.ps1
@@ -0,0 +1,11 @@
+﻿#$GPXFiles = .\Get-GPXFromPQs.ps1 -DirWithPQs C:\Users\andy\Documents\Code\cachedb\scratchpad\zip;
+$GPXFILES = $GPXFiles |sort timestamp;
+foreach ($gpx in $GPXFiles) {
+#	.\import-gpxtodb.ps1 -sqlinstance hobbes\sqlexpress -Database CacheDB -FileToImport $gpx.filepath;
+}
+
+$gpxfiles = get-childitem -path C:\Users\andy\AppData\Local\Temp\PQs;
+$GPXFILES = $GPXFiles |sort lastwritetime;
+foreach ($gpx in $GPXFiles) {
+	.\import-gpxtodb.ps1 -sqlinstance hobbes\sqlexpress -Database CacheDB -FileToImport $gpx.fullname;
+}
