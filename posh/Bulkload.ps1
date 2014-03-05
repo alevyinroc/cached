add30882be3a4737b659972d4fcd33dec54d81c5
Andy Levy <alevyinroc@users.noreply.github.com>
2014-03-04 22:45:05 -0500

    trying to sort out issues with FK constraints loading some GPX files

--- posh/Bulkload.ps1
+++ posh/Bulkload.ps1
@@ -1,5 +1,6 @@
 ﻿$GPXFiles = .\Get-GPXFromPQs.ps1 -DirWithPQs c:\users\andy\Downloads;
 $GPXFILES = $GPXFiles |sort timestamp;
 foreach ($gpx in $GPXFiles) {
+    write-output $gpx.filepath;
 	.\import-gpxtodb.ps1 -sqlinstance win81 -Database CacheDB -FileToImport $gpx.filepath;
 }
