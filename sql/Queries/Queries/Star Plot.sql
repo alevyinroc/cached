f958b4560c859b7478154be876d05400fbcccd08
Andy Levy <alevyinroc@users.noreply.github.com>
2014-04-12 08:35:15 -0400

    Import works completely!

--- sql/Queries/Queries/Star Plot.sql
+++ sql/Queries/Queries/Star Plot.sql
@@ -1,71 +1,52 @@
-declare @home geography;
-SELECT @home = latlong from CenterPoints where locationname='Home';
---SELECT locationname, @home.STDistance(latlong)/1000 as [Distance from home] from CenterPoints order by [Distance from home];
+DECLARE @home GEOGRAPHY;
 
+SELECT @home = latlong
+FROM CenterPoints
+WHERE locationname = 'Home';
+
+--SELECT locationname, @home.STDistance(latlong)/1000 as [Distance from home] from CenterPoints order by [Distance from home];
 -- But with Geography data type, we get a Great Circle plot:
 --  http://www.gcmap.com/mapui?P=ROC-SEA
 -- Shows distance of 2166 miles
-
-SELECT top 20 geography::STGeomFromText(
-	'LINESTRING ('
-	  +CAST(latlong.Long AS VARCHAR)
-	  +' '
-	  +CAST(latlong.Lat AS VARCHAR)
-	  +', '
-	  +CAST(@home.Long AS VARCHAR)
-	  +' '
-	  +CAST(@home.Lat AS VARCHAR) 
-	  +')'	  
-	  ,latlong.STSrid),
-	  @home.STDistance(latlong)/1000 as DistFromHomeKM,
-	  dbo.DistanceInMiles(@home,latlong) as DistFromHomeMiles,
-	  cacheid
+SELECT TOP 20 GEOGRAPHY::STGeomFromText('LINESTRING (' + CAST(latlong.Long AS VARCHAR) + ' ' + CAST(latlong.Lat AS VARCHAR) + ', ' + CAST(@home.Long AS VARCHAR) + ' ' + CAST(@home.Lat AS VARCHAR) + ')', latlong.STSrid)
+	,@home.STDistance(latlong) / 1000 AS DistFromHomeKM
+	,dbo.DistanceInMiles(@home, latlong) AS DistFromHomeMiles
+	,cacheid
 FROM caches
 --where dbo.Bearing(@home,latlong) between 270 and 360 and dbo.Bearing(@home,latlong) between 0 and 90
-order by @home.STDistance(latlong) asc
-;
+ORDER BY @home.STDistance(latlong) ASC;
 
-SELECT
-	c2.cachename, c2.cacheid,c2.latitude,c2.longitude, c2.latlong,logdate, logtext, c.cachername as finder, lt.logtypedesc as LogType,
-	geography::STGeomFromText(
-	'LINESTRING ('
-	  +CAST(c2.latlong.Long AS VARCHAR)
-	  +' '
-	  +CAST(c2.latlong.Lat AS VARCHAR)
-	  +', '
-	  +CAST(@home.Long AS VARCHAR)
-	  +' '
-	  +CAST(@home.Lat AS VARCHAR) 
-	  +')'	  
-	  ,c2.latlong.STSrid) as LineToCache
-from
-	caches c2
-	join cache_logs cl on c2.cacheid = cl.cacheid
-	join logs l on l.logid = cl.logid
-	JOIN log_types lt on l.logtypeid = lt.logtypeid
-	JOIN cachers c on l.cacherid = c.cacherid
-where
-	c.cachername = 'dakboy' AND lt.countsasfind = 1
-ORDER BY
-	l.logdate asc;
+SELECT c2.cachename
+	,c2.cacheid
+	,c2.latitude
+	,c2.longitude
+	,c2.latlong
+	,logdate
+	,logtext
+	,c.cachername AS finder
+	,lt.logtypedesc AS LogType
+	,GEOGRAPHY::STGeomFromText('LINESTRING (' + CAST(c2.latlong.Long AS VARCHAR) + ' ' + CAST(c2.latlong.Lat AS VARCHAR) + ', ' + CAST(@home.Long AS VARCHAR) + ' ' + CAST(@home.Lat AS VARCHAR) + ')', c2.latlong.STSrid) AS LineToCache
+FROM caches c2
+JOIN cache_logs cl ON c2.cacheid = cl.cacheid
+JOIN logs l ON l.logid = cl.logid
+JOIN log_types lt ON l.logtypeid = lt.logtypeid
+JOIN cachers c ON l.cacherid = c.cacherid
+WHERE c.cachername = 'dakboy'
+	AND lt.countsasfind = 1
+ORDER BY l.logdate ASC;
 
-declare @home2 geometry;
+DECLARE @home2 geometry;
 
-SELECT Latitude,Longitude from CenterPoints where locationname='Home';
-SET @home2 = [geometry]::Point(-77.306933,42.885983,0);
+SELECT Latitude
+	,Longitude
+FROM CenterPoints
+WHERE locationname = 'Home';
+
+SET @home2 = [geometry]::Point(- 77.306933, 42.885983, 0);
+
 -- TODO: Do distance with geometry instead of geography
-SELECT top 20 geometry::STGeomFromText(
-	'LINESTRING ('
-	  +CAST(longitude AS VARCHAR)
-	  +' '
-	  +CAST(latitude AS VARCHAR)
-	  +', '
-	  +CAST(-77.306933 AS VARCHAR)
-	  +' '
-	  +CAST(42.885983 AS VARCHAR) 
-	  +')'	  
-	  ,0),
-	  cacheid
+SELECT TOP 20 geometry::STGeomFromText('LINESTRING (' + CAST(longitude AS VARCHAR) + ' ' + CAST(latitude AS VARCHAR) + ', ' + CAST(- 77.306933 AS VARCHAR) + ' ' + CAST(42.885983 AS VARCHAR) + ')', 0)
+	,cacheid
+	--,@home2.STDistance(latlong) AS from_home
 FROM caches
-order by cacheid
-;
+ORDER BY cacheid;
