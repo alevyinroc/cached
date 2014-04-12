5981cacbf3163b03ada74b39e6a74370e3c0bbd9
Andy Levy <andy@levyclan.us>
2014-04-12 08:59:41 -0400

    Tweak performance tests

--- sql/Queries/Queries/Performance Comparisons.sql
+++ sql/Queries/Queries/Performance Comparisons.sql
@@ -1,20 +1,30 @@
-SET NOCOUNT OFF;
-SET STATISTICS IO ON;
-SET statistics TIME ON;
---select latitude,longitude from Geocaches.dbo.caches;
-select COUNT(*) from CacheDB.dbo.caches;
+dbcc freeproccache;;
+dbcc dropcleanbuffers;
 
-insert INTO PerformanceCompare.dbo.Computedcolumn (latitude,longitude) select latitude,longitude from Geocaches.dbo.caches;
-insert INTO PerformanceCompare.dbo.ComputedColumnPersist (latitude,longitude) select latitude,longitude from Geocaches.dbo.caches;
-insert INTO PerformanceCompare.dbo.NoGeoColumn (latitude,longitude) select latitude,longitude from Geocaches.dbo.caches;
-insert INTO PerformanceCompare.dbo.TriggerGeoColumn (latitude,longitude) select latitude,longitude from Geocaches.dbo.caches;
+SET NOCOUNT OFF;
+--SET STATISTICS IO ON;
+--SET statistics TIME ON;
+----select latitude,longitude from Geocaches.dbo.caches;
+--select COUNT(*) from CacheDB.dbo.caches;
 SET STATISTICS IO OFF;
 SET statistics TIME Off;
+insert INTO PerformanceCompare.dbo.Computedcolumn (latitude,longitude) select latitude,longitude from CacheDB.dbo.caches;
+insert INTO PerformanceCompare.dbo.ComputedColumnPersist (latitude,longitude) select latitude,longitude from CacheDB.dbo.caches;
+insert INTO PerformanceCompare.dbo.NoGeoColumn (latitude,longitude) select latitude,longitude from CacheDB.dbo.caches;
+insert INTO PerformanceCompare.dbo.TriggerGeoColumn (latitude,longitude) select latitude,longitude from CacheDB.dbo.caches;
+--SET STATISTICS IO OFF;
+--SET statistics TIME Off;
 
+SET STATISTICS IO ON;
+--SET statistics TIME ON;
+print 'computed'
 SELECT latlong from PerformanceCompare.dbo.ComputedColumn;
+print 'computedpersist';
 SELECT latlong from PerformanceCompare.dbo.ComputedColumnPersist;
+print 'trigger';
 SELECT latlong from PerformanceCompare.dbo.TriggerGeoColumn;
 /* Aha! The trigger doesn't work with the bulk-insert I'm doing above! */
+print 'nogeo';
 SELECT performancecompare.dbo.GeoPoint(latitude,longitude) from PerformanceCompare.dbo.NoGeoColumn;
 /*TRUNCATE table PerformanceCompare.dbo.computedcolumn;
 TRUNCATE table PerformanceCompare.dbo.computedcolumnpersist;
