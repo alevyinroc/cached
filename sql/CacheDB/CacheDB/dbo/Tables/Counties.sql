98667ea6bc2c368c1c362fe374597d09d40cd8bc
Andy Levy <andy@levyclan.us>
2014-04-25 14:44:29 -0400

    New county table re #46 & #51

---
+++ sql/CacheDB/CacheDB/dbo/Tables/Counties.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Counties]
+(
+	[CountyId] INT NOT NULL DEFAULT next value for CountyId, 
+    [CountyName] NVARCHAR(50) NOT NULL, 
+    [StateId] INT NOT NULL, 
+	CONSTRAINT [PK_Counties] PRIMARY KEY CLUSTERED ([CountyId] ASC),
+    CONSTRAINT [FK_Counties_States] FOREIGN KEY ([StateId]) REFERENCES [States]([StateId])
+)
