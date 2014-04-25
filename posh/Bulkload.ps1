4b0ea14d424ce56b1a48119faf3dbd7b0a5c3594
Andy Levy <andy@levyclan.us>
2014-04-25 14:10:05 -0400

    Revised where-object syntax. fixed bulk load status messages. removed dependence upon sqlps. Cleaned up cmdlet naming

--- posh/Bulkload.ps1
+++ posh/Bulkload.ps1
@@ -8,14 +8,14 @@ $wptfiles = $GPXFiles|Where-Object {$_.filepath -like "*-wpts.gpx"};
 
 foreach ($gpx in $cachefiles) {
     $filesprocessed++;
-    Write-Progress -Activity "Processing cache files" -Status "Filename $($gpx.basename)" -Id 1 -PercentComplete $(($filesprocessed/$cachefiles.Count)*100);
+    Write-Progress -Activity "Processing cache files" -Status "Filename $($gpx.name)" -Id 1 -PercentComplete $(($filesprocessed/$cachefiles.Count)*100);
     write-output $gpx.filepath;
-	.\import-gpxtodb.ps1 -sqlinstance win81 -Database CacheDB -FileToImport $gpx.filepath;
+#	.\import-gpxtodb.ps1 -sqlinstance win81 -Database CacheDB -FileToImport $gpx.filepath;
 }
 $filesprocessed = 0;
 foreach ($gpx in $wptfiles) {
     $filesprocessed++;
-    Write-Progress -Activity "Processing cache files" -Status "Filename $($gpx.basename)" -Id 1 -PercentComplete $(($filesprocessed/$wptfiles.Count)*100);
+    Write-Progress -Activity "Processing waypoint files" -Status "Filename $($gpx.name)" -Id 1 -PercentComplete $(($filesprocessed/$wptfiles.Count)*100);
     write-output $gpx.filepath;
 	.\import-gpxtodb.ps1 -sqlinstance win81 -Database CacheDB -FileToImport $gpx.filepath;
 }
--- sql/Queries/Queries/Limit Number of Logs Per Cache.sql
+++ sql/Queries/Queries/Limit Number of Logs Per Cache.sql
@@ -19,6 +19,14 @@ from
 where
 	--c2.cacheid = 'GC38VEC'
 	@home.STDistance(c2.latlong)/1000 <= 50
+	and c2.cacheid not in (SELECT c2.cacheid
+FROM logs l
+JOIN cachers c ON c.cacherid = l.cacherid
+JOIN log_types lt ON l.logtypeid = lt.logtypeid
+JOIN cache_logs c2 ON c2.logid = l.logid
+JOIN caches c3 ON c2.cacheid = c3.cacheid
+WHERE c.cachername = 'dakboy'
+	AND lt.countsasfind = 1)
 /*ORDER BY
 	@home.STDistance(c2.latlong),
 	l.logdate DESC*/
