c5b77412419a95fac142d8e70014bb763b99629a
Andy Levy <andy@levyclan.us>
2014-04-26 11:39:47 -0400

    Resume bulk loading of cache files

--- posh/Bulkload.ps1
+++ posh/Bulkload.ps1
@@ -10,7 +10,7 @@ foreach ($gpx in $cachefiles) {
     $filesprocessed++;
     Write-Progress -Activity "Processing cache files" -Status "Filename $($gpx.name)" -Id 1 -PercentComplete $(($filesprocessed/$cachefiles.Count)*100);
     write-output $gpx.filepath;
-#	.\import-gpxtodb.ps1 -sqlinstance win81 -Database CacheDB -FileToImport $gpx.filepath;
+	.\import-gpxtodb.ps1 -sqlinstance win81 -Database CacheDB -FileToImport $gpx.filepath;
 }
 $filesprocessed = 0;
 foreach ($gpx in $wptfiles) {
