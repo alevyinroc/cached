503bde962dfc30cfdb3851fcd42197978398d21a
Andy Levy <alevyinroc@users.noreply.github.com>
2014-04-26 15:01:46 -0400

    Cleanup

--- sql/Queries/Queries/Centroid.sql
+++ sql/Queries/Queries/Centroid.sql
@@ -1,10 +1,15 @@
-create table #myfinds (
-findnum int primary key,
-latitude decimal(8,6),
-longitude decimal(9,6)
-);
-insert into #myfinds
-SELECT row_number() over (order by l.logdate asc) as FindNum,c3.latitude,c3.longitude
+CREATE TABLE #myfinds (
+	findnum INT PRIMARY KEY
+	,latitude DECIMAL(8, 6)
+	,longitude DECIMAL(9, 6)
+	);
+
+INSERT INTO #myfinds
+SELECT row_number() OVER (
+		ORDER BY l.logdate ASC
+		) AS FindNum
+	,c3.latitude
+	,c3.longitude
 FROM logs l
 JOIN cachers c ON c.cacherid = l.cacherid
 JOIN log_types lt ON l.logtypeid = lt.logtypeid
@@ -12,20 +17,20 @@ JOIN cache_logs c2 ON c2.logid = l.logid
 JOIN caches c3 ON c2.cacheid = c3.cacheid
 WHERE c.cachername = 'dakboy'
 	AND lt.countsasfind = 1
-ORDER BY l.logdate asc;
+ORDER BY l.logdate ASC;
 
 /*Parameters for WGS84 ellipsoid
 @ecc is eccentricity squared*/
 DECLARE @axis INT = 6378137;
-DECLARE @ecc float = 0.00669438038;
+DECLARE @ecc FLOAT = 0.00669438038;
 /*
 x = longitude
 y = latitude
 z = altitude?
 */
-DECLARE @x float = 0;
-DECLARE @y float = 0;
-DECLARE @z float = 0;
+DECLARE @x FLOAT = 0;
+DECLARE @y FLOAT = 0;
+DECLARE @z FLOAT = 0;
 
 -- x
 SELECT @x = AVG((@axis / (SQRT(1 - (@ecc * SIN(RADIANS(Latitude + 0))) * SIN(RADIANS(Latitude + 0))))) * (COS(RADIANS(latitude + 0)) * SIN(RADIANS(longitude + 0))))
@@ -57,29 +62,30 @@ DECLARE @v FLOAT;
 
 SELECT @v = @axis / (Sqrt(1 - (@ecc * Sin(@LatitudeN) * Sin(@LatitudeN))))
 
-DECLARE @errvalue float = 1;
-DECLARE @tmpN float = 0;
-DECLARE @tmpM float = 0;
+DECLARE @errvalue FLOAT = 1;
+DECLARE @tmpN FLOAT = 0;
+DECLARE @tmpM FLOAT = 0;
 
 WHILE @errvalue > 0.000001
 	AND @tmpM < 5
 BEGIN
 	SELECT @tmpM = @tmpM + 1;
-	print @tmpm;
+
 	SELECT @x = (@ecc * @v * Sin(@LatitudeN));
-	print @x;
+
 	SELECT @tmpN = ATN2(@z + @x, @y);
-	print @tmpn;
+
 	SELECT @errvalue = Abs(@tmpN - @LatitudeN);
-	print @errvalue;
+
 	SELECT @LatitudeN = @tmpN;
 END
-print @latituden;
+
 -- Convert from radians to degrees and format as decimal minutes
 SELECT @LatitudeN = 180 * @LatitudeN / PI();
 
 SELECT @longitudeN = 180 * @longitudeN / PI();
 
-select @latitudeN,@longitudeN;
+SELECT @latitudeN
+	,@longitudeN;
 
-drop table #myfinds;
+DROP TABLE #myfinds;
