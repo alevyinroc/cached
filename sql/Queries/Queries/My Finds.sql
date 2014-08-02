5ebd45889c4d2dfef3f115f63a0d076484df8503
Andy Levy <andy@levyclan.us>
2014-08-01 20:48:00 -0400

    Final query sync to repo before moving to new VM

--- sql/Queries/Queries/My Finds.sql
+++ sql/Queries/Queries/My Finds.sql
@@ -26,3 +26,40 @@ join countries c4 on c3.CountryId = c4.CountryId
 WHERE c.cachername = 'dakboy'
 	AND lt.countsasfind = 1
 ORDER BY l.logdate asc;
+
+-- For GC47NEJ
+
+SELECT c3.cachename
+	,c2.cacheid
+into #foundmultis
+FROM logs l
+JOIN cachers c ON c.cacherid = l.cacherid
+JOIN log_types lt ON l.logtypeid = lt.logtypeid
+JOIN cache_logs c2 ON c2.logid = l.logid
+JOIN caches c3 ON c2.cacheid = c3.cacheid
+join point_types p on c3.typeid = p.typeid
+WHERE c.cachername = 'dakboy'
+	AND lt.countsasfind = 1
+	and p.typename = 'Multi-Cache';
+
+select top 1 * from #foundmultis where cachename like 'm%' 
+union
+select top 1 * from #foundmultis where cachename like 'u%' 
+union
+select top 1 * from #foundmultis where cachename like 'l%' 
+union
+select top 1  * from #foundmultis where cachename like 't%' and cachename not like 'the%' 
+union
+select top 1  * from #foundmultis where cachename like 'i%' 
+union
+select top 1 * from #foundmultis where cachename like 'c%' 
+union
+select top 1 * from #foundmultis where cachename like 'a%' and cachename not like 'a %' and cachename <> 'a' 
+union
+select top 1 * from #foundmultis where cachename like 'c%' 
+union
+select top 1 * from #foundmultis where cachename like 'h%' 
+union
+select top 1 * from #foundmultis where cachename like 'e%' 
+;
+drop table #foundmultis;
