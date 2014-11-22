ed11348398e7af36d685400dd6410b5c0b28fae9
Andy Levy <andy@levyclan.us>
2014-11-22 20:16:48 +0000

    Queries for updating status & elevation from GSAK

---
+++ sql/Queries/Queries/Archive Caches.sql
@@ -0,0 +1,5 @@
+-- Locate caches which aren't in the GSAK database
+select c.cacheid,c.cachename, s.Name,c.lastupdated from caches c left outer join (select cast(code as varchar) code,state from OPENQUERY(GSAKMain, 'select code,state from caches')) g on c.cacheid = g.code join states s on s.StateId = c.StateId join statuses st on c.cachestatus = st.statusid where st.statusname <> 'Archived' and  g.code is null and s.name = 'new york' order by c.lastupdated;
+
+-- Archive all caches not found in GSAK
+update  c set c.cachestatus = 2 from caches c left outer join (select cast(code as varchar) code,state from OPENQUERY(GSAKMain, 'select code,state from caches')) g on c.cacheid = g.code join states s on s.StateId = c.StateId  join statuses st on c.cachestatus = st.statusid where st.statusname <> 'Archived' and g.code is null and s.name = 'new york';
