824624a562776bb1c80ca9d1f941c44ebef15ae3
Andy Levy <alevyinroc@users.noreply.github.com>
2014-11-28 22:22:02 +0000

    Formatting

--- sql/Queries/Queries/Update Elevations from GSAK.sql
+++ sql/Queries/Queries/Update Elevations from GSAK.sql
@@ -1,11 +1,32 @@
-use CacheDB;
+USE CacheDB;
 
-select c.cacheid,c.elevation as my_elevation,
---g.elevation * 0.3048 as gsak_elevation
-g.elevation as gsak_elevation
- from caches c  join (select cast(code as varchar) code,elevation from OPENQUERY(GSAKMain, 'select code,elevation from caches')) g on c.cacheid = g.code join states s on s.StateId = c.StateId where c.elevation = 0  and s.name = 'new york' order by gsak_elevation;
+SELECT c.cacheid
+	,c.elevation AS my_elevation
+	,
+	--g.elevation * 0.3048 as gsak_elevation
+	g.elevation AS gsak_elevation
+FROM caches c
+JOIN (
+	SELECT cast(code AS VARCHAR) code
+		,elevation
+	FROM OPENQUERY(GSAKMain, 'select code,elevation from caches')
+	) g ON c.cacheid = g.code
+JOIN states s ON s.StateId = c.StateId
+WHERE c.elevation = 0
+	AND s.NAME = 'new york'
+ORDER BY gsak_elevation;
 
-update c set
---c.elevation = g.elevation * 0.3048
-c.elevation = g.elevation
-from caches c join (select cast(code as varchar) code,elevation from OPENQUERY(GSAKMain, 'select code,elevation from caches')) g on c.cacheid = g.code join states s on s.StateId = c.StateId where c.elevation = 0 and g.elevation is not null and s.name = 'new york';
+UPDATE c
+SET
+	--c.elevation = g.elevation * 0.3048
+	c.elevation = g.elevation
+FROM caches c
+JOIN (
+	SELECT cast(code AS VARCHAR) code
+		,elevation
+	FROM OPENQUERY(GSAKMain, 'select code,elevation from caches')
+	) g ON c.cacheid = g.code
+JOIN states s ON s.StateId = c.StateId
+WHERE c.elevation = 0
+	AND g.elevation IS NOT NULL
+	AND s.NAME = 'new york';
