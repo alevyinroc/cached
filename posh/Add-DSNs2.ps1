cc9bf8c16a5b4eab02f338bcd84c023dc1fad8d3
Andy Levy <alevyinroc@users.noreply.github.com>
2014-11-29 20:02:37 +0000

    Add DSNs for blog post

---
+++ posh/Add-DSNs2.ps1
@@ -0,0 +1,6 @@
+﻿
+get-childitem C:\users\andy\Dropbox\gsak8\data -recurse -filter sqlite.db3|Select-Object -ExpandProperty fullname|ForEach-Object {
+$DSNName = $_.split("\")[6];
+Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
+};
+Get-OdbcDsn
