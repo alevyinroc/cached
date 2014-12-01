81fd01273a5ccc8b67e7ed35e6f0957def14cc43
Andy Levy <andy@levyclan.us>
2014-11-30 21:18:26 -0500

    Format code

--- posh/Add-DSNs2.ps1
+++ posh/Add-DSNs2.ps1
@@ -1,6 +1,5 @@
-﻿
-get-childitem C:\users\andy\Dropbox\gsak8\data -recurse -filter sqlite.db3|Select-Object -ExpandProperty fullname|ForEach-Object {
-$DSNName = $_.split("\")[6];
-Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
+﻿Get-ChildItem -Path C:\users\andy\Dropbox\gsak8\data -Recurse -Filter sqlite.db3|Select-Object -ExpandProperty FullName | ForEach-Object {
+	$DSNName = $_.split("\")[6];
+	Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
 };
-Get-OdbcDsn
+Get-OdbcDsn -DriverName "SQLite3 ODBC Driver";
