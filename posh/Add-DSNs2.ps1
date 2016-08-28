75c4ab0d602fa1f230fe96a7dfd4743a4a6da13f
Andy Levy <andy@levyclan.us>
2016-08-28 14:04:27 +0000

    Script cleanup
    Formatting fixed up

--- posh/Add-DSNs2.ps1
+++
@@ -1,6 +0,0 @@
-﻿Get-ChildItem -Path C:\users\andy\Dropbox\gsak8\data -Recurse -Filter sqlite.db3|Select-Object -ExpandProperty FullName | ForEach-Object {
-	$DSNName = $_.split("\")[6];
-	Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
-};
-Get-OdbcDsn -DriverName "SQLite3 ODBC Driver" | Format-Table -AutoSize;
-Get-OdbcDsn -DriverName "SQLite3 ODBC Driver" | Format-Table -AutoSize | Out-File -FilePath C:\Users\andy\Documents\cachedb\createdDSNs.txt;
--- posh/Add-DSNs3.ps1
+++ posh/Add-DSNs3.ps1
@@ -15,24 +15,26 @@
         $ServerInstance = ".\sql2016"
     )
 
-    Begin
-    {
-    }
-    Process
-    {
-    Get-OdbcDsn | Where-Object {$_.name -notlike "sqlite*"}|Remove-OdbcDsn;
+Begin
+{
+}
+Process
+{
+    Get-OdbcDsn | Where-Object {$_.Name -notlike "sqlite*"} | Remove-OdbcDsn;
     Get-ChildItem -Path $GSAKDataDirectory -Recurse -Filter sqlite.db3|Select-Object -ExpandProperty FullName | ForEach-Object {
         $DSNName = $_.split("\")[6];
         Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
     };
- 
+
     $AllDSNs = Get-OdbcDsn -DriverName "SQLite3 ODBC Driver"|where-object {$_.name -notlike "sqlite*"};
     foreach ($DSN in $AllDSNs) {
         $CreateLinkedServerSP =@"
-    EXEC master.dbo.sp_addlinkedserver @server = N'$($DSN.Name)', @srvproduct=N'$($DSN.Name)', @provider=N'MSDASQL', @datasrc=N'$($DSN.Name)';
-    EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'$($DSN.Name)',@useself=N'False',@locallogin=NULL,@rmtuser=NULL,@rmtpassword=NULL;
+            EXEC master.dbo.sp_addlinkedserver @server = N'$($DSN.Name)', @srvproduct=N'$($DSN.Name)', @provider=N'MSDASQL', @datasrc=N'$($DSN.Name)';
+            EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'$($DSN.Name)',@useself=N'False',@locallogin=NULL,@rmtuser=NULL,@rmtpassword=NULL;
 "@;
-        invoke-sqlcmd -query $CreateLinkedServerSP -serverinstance $ServerInstance -database master;
+            invoke-sqlcmd -query $CreateLinkedServerSP -serverinstance $ServerInstance -database master;
     }
 }
-End {}
+End
+{
+}
