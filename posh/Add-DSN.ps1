a729ddb405a8da1f68b2cb5a6d81ba5713d39142
= <andy@levyclan.us>
2020-01-05 08:32:23 -0500

    Get up to more modern standards

--- posh/Add-DSN.ps1
+++ posh/Add-DSN.ps1
@@ -1,34 +1,32 @@
-﻿#require -version 3
-#require -module sqlserver
-Param
-(
-    [Parameter(Mandatory=$true,Position=0)]
-    [ValidateScript({Test-Path -type container -path $_})]
-    $GSAKDataDirectory = "C:\Users\andy\Dropbox\GSAK8\data",
-    [Parameter(Mandatory=$true,Position=1)]
-    $ServerInstance = ".\sql2016"
-)
+﻿import-module dbatools
+function Add-GcDsn {
+    Param
+    (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateScript( { Test-Path -type container -path $_ })]
+        [string]$GSAKDataDirectory = "C:\Users\Andy\OneDrive\GSAK8\data",
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string]$SqlInstance = "localhost\sql17"
+    )
 
-Begin
-{
-}
-Process
-{
-    Get-OdbcDsn | Where-Object {$_.Name -notlike "sqlite*"} | Remove-OdbcDsn;
-    Get-ChildItem -Path $GSAKDataDirectory -Recurse -Filter sqlite.db3|Select-Object -ExpandProperty FullName | ForEach-Object {
-        $DSNName = $_.split("\")[6];
-        Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
-    };
+    Begin {
+    }
+    Process {
+        Get-OdbcDsn | Where-Object { $_.Name -notlike "sqlite*" } | Remove-OdbcDsn;
+        Get-ChildItem -Path $GSAKDataDirectory -Recurse -Filter sqlite.db3 | Select-Object -ExpandProperty FullName | ForEach-Object {
+            $DSNName = $_.split("\")[6];
+            Add-OdbcDsn -Name $DSNName -DriverName "SQLite3 ODBC Driver" -Platform 64-bit -DsnType System -SetPropertyValue "Database=$_";
+        };
 
-    $AllDSNs = Get-OdbcDsn -DriverName "SQLite3 ODBC Driver"|where-object {$_.name -notlike "sqlite*"};
-    foreach ($DSN in $AllDSNs) {
-        $CreateLinkedServerSP =@"
+        $AllDSNs = Get-OdbcDsn -DriverName "SQLite3 ODBC Driver" | where-object { $_.name -notlike "sqlite*" };
+        foreach ($DSN in $AllDSNs) {
+            $CreateLinkedServerSP = @"
             EXEC master.dbo.sp_addlinkedserver @server = N'$($DSN.Name)', @srvproduct=N'$($DSN.Name)', @provider=N'MSDASQL', @datasrc=N'$($DSN.Name)';
             EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'$($DSN.Name)',@useself=N'False',@locallogin=NULL,@rmtuser=NULL,@rmtpassword=NULL;
 "@;
-            invoke-sqlcmd -query $CreateLinkedServerSP -serverinstance $ServerInstance -database master;
+            Invoke-DbaQuery -query $CreateLinkedServerSP -sqlinstance $SqlInstance -database master;
+        }
     }
-}
-End
-{
+    End {
+    }
 }
